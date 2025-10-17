921194adfd62f2f97796c7639f594e482dd69650
kolya5544 <kolya@iktm.me>
2025-10-17 06:38:39 +0000

    Add Docker Compose and Dockerfiles required

---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+var client_1 = require("react-dom/client");
+var App_1 = require("./App");
+var react_router_dom_1 = require("react-router-dom");
+require("./index.css");
+client_1.default.createRoot(document.getElementById("root")).render(react_1.default.createElement(react_1.default.StrictMode, null,
+    react_1.default.createElement(react_router_dom_1.BrowserRouter, null,
+        react_1.default.createElement(App_1.default, null))));
